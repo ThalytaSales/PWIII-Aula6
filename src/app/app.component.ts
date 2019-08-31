04cc7ff9225337baf80b4714d305c29bb92d5826
Thalyta Sales <Thalyta.sales23@gmail.com>
2019-08-30 21:54:16 -0300

    data bingding Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'etec-base';
+  peso = 70 ;
+  altura  = 1.70 ;
+  
+
+  exibir() : void {
+    let imc = this.peso / (this.altura * this.altura);
+    alert("IMC é: " + imc) 
+  }
 }
